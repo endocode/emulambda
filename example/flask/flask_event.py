2ed6db932be10b6f9bc79e9b1cd9c308340558b2
Dongsu Park <dongsu@endocode.com>
2017-03-06 13:05:03 +0100

    example: make flask_event actually handle GET, POST requests

--- example/flask/flask_event.py
+++ example/flask/flask_event.py
@@ -1,19 +1,81 @@
+import sys
+
+from urllib import urlencode
+from StringIO import StringIO
+
 from flask import Flask
+from werkzeug.wrappers import BaseRequest
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/lambda_handler', methods=['GET', 'POST'])
+
 def lambda_handler(event=None, context=None):
-    print "Lambda function invoked /"
+    print "Lambda function invoked /lambda_handler"
     response = LambdaResponse()
 
-    return {
-        'statusCode': response.status,
-        'headers': response.response_headers,
-        'body': "ok"
-    }
+    reqMethod = event['httpMethod']
+    if reqMethod == 'POST':
+        print "Method: %s" % reqMethod
+        qsp = event['queryStringParameters']
+        qs = urlencode(qsp) if qsp else ''
+        return qs, 200
+    elif reqMethod == 'GET':
+        print "Method: %s" % reqMethod
 
-class LambdaResponse(object):
+        body = next(app.wsgi_app(
+            make_environ(event),
+            response.start_response
+        ))
+
+        return {
+            'statusCode': response.status,
+            'headers': response.response_headers,
+            'body': body
+        }
+
+def make_environ(event):
+    environ = {}
+
+    for hdr_name, hdr_value in event['headers'].items():
+        hdr_name = hdr_name.replace('-', '_').upper()
+        if hdr_name in ['CONTENT_TYPE', 'CONTENT_LENGTH']:
+            environ[hdr_name] = hdr_value
+            continue
+
+        http_hdr_name = 'HTTP_%s' % hdr_name
+        environ[http_hdr_name] = hdr_value
+
+    qs = event['queryStringParameters']
+
+    environ['REQUEST_METHOD'] = event['httpMethod']
+    environ['PATH_INFO'] = event['path']
+    environ['QUERY_STRING'] = urlencode(qs) if qs else ''
+    environ['REMOTE_ADDR'] = event['requestContext']['identity']['sourceIp']
+    environ['HOST'] = '%(HTTP_HOST)s:%(HTTP_X_FORWARDED_PORT)s' % environ
+    environ['SCRIPT_NAME'] = ''
+
+    environ['SERVER_PORT'] = environ['HTTP_X_FORWARDED_PORT']
+    environ['SERVER_PROTOCOL'] = 'HTTP/1.1'
+
+    environ['CONTENT_LENGTH'] = str(
+        len(event['body']) if event['body'] else ''
+    )
+
+    environ['wsgi.url_scheme'] = environ['HTTP_X_FORWARDED_PROTO']
+    environ['wsgi.input'] = StringIO(event['body'] or '')
+    environ['wsgi.version'] = (1, 0)
+    environ['wsgi.errors'] = sys.stderr
+    environ['wsgi.multithread'] = False
+    environ['wsgi.run_once'] = True
+    environ['wsgi.multiprocess'] = False
+
+    BaseRequest(environ)
+
+    return environ
+
+
+class LambdaResponse(Flask):
     def __init__(self):
         self.status = None
         self.response_headers = None
